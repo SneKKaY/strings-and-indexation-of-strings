9567f4e038612e8c99143a19f05b0beecd92d618
SneKKaY <120265382+SneKKaY@users.noreply.github.com>
2024-10-24 16:31:07 +0300

    Add files via upload

---
+++ Strings and indexation of stings/Strings.py
@@ -0,0 +1,7 @@
+print(len('alrse  paewoepsjokmten!'))
+example ='Hzi ,  zyhoaup  alrse  paewoepsjokmten!'
+print(example[0])
+print(example[-1])
+print(example[16:])
+print(example[::-1])
+print(example[::2])
